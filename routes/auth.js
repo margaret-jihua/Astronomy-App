29f13cb63e3703e04e192514389c1836810e7a0c
margare-jihua <margaret.jihua@gmail.com>
2020-08-13 14:05:26 -0700

    Signup page mocha auth.test.js 4 passing

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +9,31 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then (([user, created]) => {
+    if (created){
+      console.log(`${user.name} was created`);
+      res.redirect('/')
+    }
+    else {
+      // email exist
+      console.log('email already exist');
+      res.redirect('/auth/signup')
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/auth/signup')
+  })
+})
 
 module.exports = router;
